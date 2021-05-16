150c3eae293b9fdd7152df2f4c56eaaa73f9082f
nghia <ntnghia1908@gmail.com>
2021-05-16 11:13:05 +0700

    Completed using layout

--- src/main/java/cseiu/abet/controller/CourseController.java
+++ src/main/java/cseiu/abet/controller/CourseController.java
@@ -24,7 +24,7 @@ public class CourseController {
     public String getAllCourseFromDB(Model model) {
         List<Course> courses = courseService.getAllCourse();
         model.addAttribute("courses", courses);
-        return  "course";
+        return "admin/course-detail";
     }
 
     @Transactional
@@ -32,7 +32,7 @@ public class CourseController {
     public String getByIdCourse(@PathVariable("id") String id, Model model) {
         Course course = courseService.findCourseById(id);
         model.addAttribute("course", course);
-        return  "course";
+        return "lecturer/layout/header";
     }
 
 }
