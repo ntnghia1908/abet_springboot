5baec82914d3d6aab606369077d3759af66b6daa
nghia <ntnghia1908@gmail.com>
2021-05-24 21:14:53 +0700

    add BCryptPasswordEncoder

--- src/main/java/cseiu/abet/services/AccountService.java
+++ src/main/java/cseiu/abet/services/AccountService.java
@@ -3,6 +3,8 @@ package cseiu.abet.services;
 import cseiu.abet.model.Account;
 import cseiu.abet.repo.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +23,16 @@ public class AccountService {
         this.passwordEncoder = passwordEncoder;
     }
 
+//    @Bean
+//    public DaoAuthenticationProvider authProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService);
+//        authProvider.setPasswordEncoder(encoder());
+//        return authProvider;
+//    }
+
     public Account addAccount(Account account) {
-        account.setPassword(passwordEncoder.encode(account.getPassword()));
+//        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);
     }
 
@@ -34,8 +44,11 @@ public class AccountService {
         accountRepository.deleteById(accountId);
     }
 
-    public Account checkLogin (String user_name, String password){
-        String encryptedPass = passwordEncoder.encode(password);
-        return accountRepository.checkAccount(user_name, encryptedPass);
+    public Account checkLogin (String user_name, String password) {
+//        String encryptedPass = passwordEncoder.encode(password);
+//        System.out.println(password);
+//        System.out.println(encryptedPass);
+        return accountRepository.checkAccount(user_name, password);
     }
+
 }
